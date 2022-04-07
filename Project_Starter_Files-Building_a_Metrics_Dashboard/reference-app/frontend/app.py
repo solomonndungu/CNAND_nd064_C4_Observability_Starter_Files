0f3e43978ae216dcfe0b2dcf8a344f13c7d5f5c8
solomonndungu <ssouljabee@gmail.com>
2022-04-07 10:03:47 +0300

    frontend tracing code

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,13 +1,46 @@
 from flask import Flask, render_template, request
+import threading
+import requests
+import random
+import time
 from prometheus_flask_exporter import PrometheusMetrics
+import logging
+from jaeger_client import Config
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app, group_by='endpoint')
 metrics.info("frontend_app_info", "Frontend App Info", version="1.0.3")
 
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
+
+by_endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by request endpoint',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+config = Config(
+    config={
+        'sampler': {
+            'type': 'const',
+            'param': 1,
+        },
+        'logging': True,
+    },
+    service_name='frontend'
+)
+
+tracer= config.initialize_tracer()
+
 @app.route("/")
+@by_endpoint_counter
 def homepage():
-    return render_template("main.html")
+    with tracer.start_span('html'):
+        return render_template("main.html")
 
 #metrics.register_default(
 #    metrics.counter(
